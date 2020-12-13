036f7a9164d1610d1c8b878faf038a10ff3e1bdb
Iamandi Andrei <75797799+Goobyman2@users.noreply.github.com>
2020-12-13 18:34:18 +0200

    Update Stage1-1.v

--- Stage1-1.v
+++ Stage1-1.v
@@ -103,7 +103,7 @@ Notation "S [ V /' X ]" := (update S X V) (at level 0).
 Compute (env "y").
 Compute (update (update env "y" (default)) "y" (res_bool true) "y").
 Compute (update env "a" (res_nat 100)) "a".
-
+Compute (update (update env "y" (default)) "y" (res_string "true") "y").
 
 Inductive AExp :=
   | avar: string -> AExp 
@@ -113,6 +113,72 @@ Inductive AExp :=
   | amul: AExp -> AExp -> AExp 
   | adiv: AExp -> AExp -> AExp 
   | amod: AExp -> AExp -> AExp.
+
+Notation "A +' B" := (aplus A B)(at level 50, left associativity).
+Notation "A -' B" := (amin A B)(at level 50, left associativity).
+Notation "A *' B" := (amul A B)(at level 48, left associativity).
+Notation "A /' B" := (adiv A B)(at level 48, left associativity).
+Notation "A %' B" := (amod A B)(at level 45, left associativity).
+
+Definition plus_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 + v2)
+    end.
+
+Definition sub_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num n1, num n2 => if Nat.ltb n1 n2
+                        then error_nat
+                        else num (n1 - n2)
+    end.
+
+Definition mul_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | num v1, num v2 => num (v1 * v2)
+    end.
+
+Definition div_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (Nat.div v1 v2)
+    end.
+
+Definition mod_ErrorNat (n1 n2 : ErrorNat) : ErrorNat :=
+  match n1, n2 with
+    | error_nat, _ => error_nat
+    | _, error_nat => error_nat
+    | _, num 0 => error_nat
+    | num v1, num v2 => num (v1 - v2 * (Nat.div v1 v2))
+    end.
+
+Fixpoint aeval_fun (a : AExp) (env : Env) : ErrorNat :=
+  match a with
+  | avar v => match (env v) with
+                | res_nat n => n
+                | _ => error_nat
+                end
+  | anum v => v
+  | aplus a1 a2 => (plus_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | amul a1 a2 => (mul_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | amin a1 a2 => (sub_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  | adiv a1 a2 => (div_ErrorNat  (aeval_fun a1 env) (aeval_fun a2 env))
+  | amod a1 a2 => (mod_ErrorNat (aeval_fun a1 env) (aeval_fun a2 env))
+  end.
+
+Coercion anum: ErrorNat >-> AExp.
+Coercion avar: string >-> AExp.
+
+Compute aeval_fun ("x" +' 3) (update (update env "x" (default)) "x" (res_nat 100) ).
+
+
 
 Inductive BExp :=
   | berror
@@ -125,5 +191,58 @@ Inductive BExp :=
   | band : BExp -> BExp -> BExp
   | bor : BExp -> BExp -> BExp.
 
+Notation "A <' B" := (blt A B) (at level 70).
+Notation "A >' B" := (bgt A B) (at level 70).
+Notation "!' A" := (bnot A)(at level 51, left associativity).
+Notation "A &&' B" := (band A B)(at level 52, left associativity).
+Notation "A ||' B" := (bor A B)(at level 53, left associativity).
 
-...
+Definition lt_ErrorBool (n1 n2 : ErrorNat) : ErrorBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.ltb v1 v2)
+    end.
+
+Definition gt_ErrorBool (n1 n2 : ErrorNat) : ErrorBool :=
+  match n1, n2 with
+    | error_nat, _ => error_bool
+    | _, error_nat => error_bool
+    | num v1, num v2 => boolean (Nat.ltb v2 v1)
+    end.
+
+Definition not_ErrorBool (n :ErrorBool) : ErrorBool :=
+  match n with
+    | error_bool => error_bool
+    | boolean v => boolean (negb v)
+    end.
+
+Definition and_ErrorBool (n1 n2 : ErrorBool) : ErrorBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (andb v1 v2)
+    end.
+
+Definition or_ErrorBool (n1 n2 : ErrorBool) : ErrorBool :=
+  match n1, n2 with
+    | error_bool, _ => error_bool
+    | _, error_bool => error_bool
+    | boolean v1, boolean v2 => boolean (orb v1 v2)
+    end.
+
+Fixpoint beval_fun (a : BExp) (envnat : Env) : ErrorBool :=
+  match a with
+  | btrue => true
+  | bfalse => false
+  | berror => error_bool
+  | bvar v => match (env v) with
+                | res_bool n => n
+                | _ => error_bool
+                end
+  | blt a1 a2 => (lt_ErrorBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bgt a1 a2 => (gt_ErrorBool (aeval_fun a1 envnat) (aeval_fun a2 envnat))
+  | bnot b1 => (not_ErrorBool (beval_fun b1 envnat))
+  | band b1 b2 => (and_ErrorBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  | bor b1 b2 => (or_ErrorBool (beval_fun b1 envnat) (beval_fun b2 envnat))
+  end.
